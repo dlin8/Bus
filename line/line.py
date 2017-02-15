63fe4218820494c263a1cd23ebd3ac8d0e9dfcde
dlin8 <dlin8@stuy.edu>
2017-02-15 15:33:25 -0500

    .

--- line/line.py
+++ line/line.py
@@ -4,8 +4,8 @@ import random
 
 width = 500
 height = 500
-width = int(input("width of image: "))
-height = int(input("height of image: "))
+#width = int(input("width of image: "))
+#height = int(input("height of image: "))
 colorDepth = 255
 matrix = []
 color = [0, 255, 0]
@@ -29,8 +29,28 @@ def plot(matrix, x, y, color):
     matrix[x][y][1] = color[1]
 
 def drawLine(matrix, a, b, color):
-    A = a[1] - b[1]
+    #Swap points a and b if a is to the right of b.
+    if(b[0] < a[0]):
+        b[0] = b[0] + a[0]
+        b[1] = b[1] + a[1]
+        a[0] = b[0] - a[0]
+        a[1] = b[1] - a[1]
+        b[0] = b[0] - a[0]
+        b[1] = b[1] - a[1]
     B = -1 * (b[0] - a[0])
+
+    if(a[1] >= b[1]):
+        #Octant I, II
+        A = a[1] - b[1]
+        if(A >= (-1 * B)):
+
+        else:
+
+    else:
+        #Octant VII, VIII
+    #Octant I
+    A = a[1] - b[1]
+
     d = (2 * A) + B
     x = a[0]
     y = a[1]
@@ -42,6 +62,50 @@ def drawLine(matrix, a, b, color):
         x = x + 1
         d = d + A
     plot(matrix, b[0], b[1], color)
+
+    #Octant II
+    A = a[1]- b[1]
+
+    d = A + (2 * B)
+    x = a[0]
+    y = a[1]
+    while(y > b[1]):
+        plot(matrix, x, y, color)
+        if(d < 0):
+            x = x + 1
+            d = d + A
+        y = y - 1
+        d = d + B
+    plot(matrix, b[0], b[1], color)
+
+    #Octant VII
+    A = b[1] - a[1]
+
+    d = A - (2 * B)
+    x = a[0]
+    y = a[1]
+    while(y < b[1]):
+        plot(matrix, x, y color)
+        if(d > 0):
+            x = x + 1
+            d = d + A
+        y = y - 1
+        d = d - B
+    plot(matrix, b[0], b[1], color)
+
+    #Octant VIII
+    A =  b[1] - a[1]
+
+    d = (2 * A) - B
+    x = a[0]
+    y = a[1]
+    while(x < b[0]):
+        plot(matrix, x, y, color)
+        if(d < 0):
+            y = y - a
+            d = d - B
+        x = x + 1
+        d = d + A
 
 def drawRandLine():
     randColor = [random.randrange(0,256), random.randrange(0,256), random.randrange(0,256)]
