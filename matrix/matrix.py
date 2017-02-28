1d1d11194609ded40d91eeb342934fc6fcc9c587
dlin8 <dlin8@stuy.edu>
2017-02-28 15:34:46 -0500

    .

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -19,14 +19,27 @@ def scalarMultiplication(scalar, matrix):
 
 # This function is specific to 4xN edge matrix multiplied BY a 4x4 matrix
 def matrixMultiplication(matrix, edgeMatrix):
+    if len(matrix[0]) != len(edgeMatrix):
+        print('matrices cannot be multiplied.')
+        return false
     retMatrix = []
-
+    tempList = []
+    for row in range( len(matrix) ):
+        retMatrix.append([])
+        for column in range( len(edgeMatrix[row]) ):
+            for i in range( len(edgeMatrix) ):
+                tempList.append(edgeMatrix[column][i])
+            retMatrix[row].append(dotProduct(matrix[row], tempList))
+            tempList = []
+            
+                                  
 def dotProduct(list1, list2):
     dotProduct = 0
-    if( len(list1) != len(list2) )
+    if len(list1) != len(list2):
+        print('lists of unequal lengths')
         return false
     for i in range(0, len(list1)):
-        dotProduct = dotProdcut + (list1[i] * list2[i])
+        dotProduct = dotProduct + (list1[i] * list2[i])
     return dotProduct
 
 
@@ -34,10 +47,16 @@ def dotProduct(list1, list2):
 edgeMatrix = []
 for row in range(0,4):
     edgeMatrix.append([])
-    for column in range(0,6):
-        edgeMatrix[row].append(row + column)
+    for column in range(0,4):
+        if(column == 4):
+            edgeMatrix[row].append(1)
+        else:
+            edgeMatrix[row].append(row + column)
 
 printMatrix(edgeMatrix)
 print('multiplying previous matrix with scalar value \'3\'.')
 scalarMultiplication(3, edgeMatrix)
+printMatrix(edgeMatrix)
+print('multiplying previous matrix with itself.')
+matrixMultiplication(edgeMatrix, edgeMatrix)
 printMatrix(edgeMatrix)
