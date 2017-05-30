1e0fd83a12f30a806ed793d703074596cbb0dfe8
dlin8 <dlin8@stuy.edu>
2017-05-30 15:28:30 -0400

    .quit

--- Graphics/main.py
+++ Graphics/main.py
@@ -22,8 +22,9 @@ tempScreen = screen.createScreen(4,4)
 transformMatrix = matrix.getIdentityMatrix(tempScreen)[:]
 
 def main():
-    screenOne = screen.createScreen(8, 8)
-    draw.plot(screenOne, 4, 4, green)
+    screenOne = screen.createScreen(500, 500)
+    draw.sphere(edgeMatrix, 250, 250, 0, 200, 0)
+    matrix.drawEdges(screenOne, edgeMatrix, green)
     screen.display(screenOne)
     
     #screen.saveExtension(screenOne, 'pic.png')
